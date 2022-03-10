a3fcb2291dc8951f48c419e05b752560f4d135d3
Po-Jui Chen <a00012025@gmail.com>
2022-03-10 15:53:15 +0800

    fix BscScanBlockNumberResultTransaction null

--- lib/src/models/proxy/block_by_number_model.dart
+++ lib/src/models/proxy/block_by_number_model.dart
@@ -271,7 +271,7 @@ class BscScanBlockNumberResultTransaction with EquatableMixin {
   final String hash;
   final String input;
   final String nonce;
-  final String to;
+  final String? to;
   final String transactionIndex;
   final String value;
   final String type;
@@ -288,7 +288,7 @@ class BscScanBlockNumberResultTransaction with EquatableMixin {
     required this.hash,
     required this.input,
     required this.nonce,
-    required this.to,
+    this.to,
     required this.transactionIndex,
     required this.value,
     required this.type,
@@ -393,7 +393,7 @@ class BscScanBlockNumberResultTransaction with EquatableMixin {
       hash,
       input,
       nonce,
-      to,
+      to ?? "",
       transactionIndex,
       value,
       type,
